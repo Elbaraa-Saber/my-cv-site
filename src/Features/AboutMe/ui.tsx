ffb2692a30c718a9d70c1859ee397b47fa35085e
Eblaraa <baraax01@gmail.com>
2024-06-27 10:50:55 +0700

    i edited text

--- src/Features/AboutMe/ui.tsx
+++ src/Features/AboutMe/ui.tsx
@@ -3,7 +3,6 @@ import "./style.scss";
 import ImgMain from "../../Shared/Assets/Imgs/main.jpg";
 import { Img, Skill } from "../../Shared/index.ts";
 
-
 export const AboutMe = () => {
   return (
     <section className="aboutMe">
@@ -12,9 +11,9 @@ export const AboutMe = () => {
         <h1 className="aboutMe__name">Салем ЭльБараа</h1>
         <p className="aboutMe__work">Front end developer</p>
         <p className="aboutMe__intro">
-          Я египетский студент в России, проведший последние три года, осваивая
-          русский язык и получив стипендию в Томском государственном
-          университете по специальности "Программная инженерия".
+          Я египетский студент, проживающий последние три года в России.
+          Осваиваю русский язык, получаю стипендию и обучаюсь по специальности:
+          "Пограммная инженерия".
         </p>
         <p className="aboutMe__langueges">
           Свободно владею арабским, русским и английским языками.
@@ -25,10 +24,14 @@ export const AboutMe = () => {
         </p>
         <h2 className="aboutMe__mySkills">Мои навыки</h2>
         <p className="aboutMe__techSkills">
-          <Skill skill="TypeScript" /><Skill skill="JavaScript" /><Skill skill="HTML" /><Skill skill="React" />
-          <Skill skill="SCSS" /><Skill skill="BEM" /><Skill skill="API" />
+          <Skill skill="TypeScript" />
+          <Skill skill="JavaScript" />
+          <Skill skill="HTML" />
+          <Skill skill="React" />
+          <Skill skill="SCSS" />
+          <Skill skill="BEM" />
+          <Skill skill="API" />
           <Skill skill="CSS" />
-          
         </p>
       </div>
     </section>
